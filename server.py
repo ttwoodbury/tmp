e1c6ff720cfd53beaeb0a01cdc5d4b49694e8b44
Jamie Woodbury <jamie.woodbury@gmail.com>
2015-01-22 09:07:18 -0800

    add template data binding

--- server.py
+++ server.py
@@ -3,9 +3,13 @@ app = Flask(__name__)
 
 from flask import render_template, url_for
 
+# import assignment1 as a1
+
+# data = a1.get_data()
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html', data=[1,2,3])
 
 if __name__ == "__main__":
     app.run()
